95f870aedd456a0831aeead483238f1c9c42e2a9
Tobi-Kun <40240050+NZSigourney@users.noreply.github.com>
2022-05-05 12:55:12 +0700

    BT 6/5/2022

---
+++ HTML/BT 6-4-2022/website/Js/main.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+function ages(number){
+	//var number = oninput();
+	//var brk = breaklines();
+	
+	if (number < 18){
+		alert("Tuổi nhỏ hơn 18 tuổi vui lòng thoát khỏi trang web này!")
+	}else{
+		document.write('Good')
+	}
+}
+ages(18)
