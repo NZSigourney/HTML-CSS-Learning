c6c9b9841fd978f9e03ba00c51292a18b847f7eb
Tobi-Kun <40240050+NZSigourney@users.noreply.github.com>
2022-05-30 09:27:44 +0700

    Add files via upload

---
+++ Code/BT 30.5.2022/JavaScript/login.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+function username(){
+	var arr = document.getElementsByTagName("input");
+	var username = arr[0];
+	var email = arr[1];
+	
+	if(username.length == 12){
+		if(isNaN(username)){
+			document
+		}
+	}
+}
